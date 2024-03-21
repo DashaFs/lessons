d2a18c0918957e96d702b2ae3e20c2e4efa27f4d
DashaFs <feshchenkodasha@gmail.com>
2024-03-21 18:35:49 +0200

    Added the homework for lesson 7.Functions

--- lesson7_homework.py
+++ lesson7_homework.py
@@ -1,40 +1,41 @@
-# def my_map_func(func, iterable):
-#     list =[]
-#     for i in iterable:
-#         list.append(func(i))
-#     print(list)
-#
-#
-# def square(x):
-#     return x ** 2
-#
-#
-# list_1 = [1, 3, 4, 5, 6]
-#
-# # with a function
-# my_map_func(square, list_1)
-#
-# # with lambda
-# my_map_func(lambda x: x**2, list_1)
-#
-# def my_filter_func(func, iterable):
-#     filtered_list = []
-#     for item in iterable:
-#         if func(item):
-#             filtered_list.append(item)
-#     print(filtered_list)
-#
-# my_filter_func(lambda x: x%2==0, list_1)
+#task
+def my_map_func(func, iterable):
+    list =[]
+    for i in iterable:
+        list.append(func(i))
+    print(list)
+
+
+def square(x):
+    return x ** 2
+
+
+list_1 = [1, 3, 4, 5, 6]
+
+# with a function
+my_map_func(square, list_1)
+
+# with lambda
+my_map_func(lambda x: x**2, list_1)
+
+def my_filter_func(func, iterable):
+    filtered_list = []
+    for item in iterable:
+        if func(item):
+            filtered_list.append(item)
+    print(filtered_list)
+
+my_filter_func(lambda x: x%2==0, list_1)
 
 
 #task2
 
-# def create_lambda(x):
-#     return lambda y : y**x
-#
-# function = create_lambda(3)
-# result = function(2)
-# print(result)
+def create_lambda(x):
+    return lambda y : y**x
+
+function = create_lambda(3)
+result = function(2)
+print(result)
 
 
 #task3
